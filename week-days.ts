ffb635f2f08079d9acb35196732055cbe2c6e808
Monir <monireh.taghilo@gmail.com>
2023-09-12 09:34:26 +0330

    add week-days.ts

---
+++ week-days.ts
@@ -0,0 +1,27 @@
+const weekDaysNumber: number = 10;
+
+switch (weekDaysNumber) {
+  case 0:
+    console.log("This is Saturday.");
+    break;
+  case 1:
+    console.log("This is Sunday.");
+    break;
+  case 2:
+    console.log("This is Monday.");
+    break;
+  case 3:
+    console.log("This is Tuesday.");
+    break;
+  case 4:
+    console.log("This is Wednesday.");
+    break;
+  case 5:
+    console.log("This is Thursday.");
+    break;
+  case 6:
+    console.log("This is Friday.");
+    break;
+  default:
+    console.log("Number is Not Valid");
+}
