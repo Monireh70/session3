fbc952921f2b216a78fd18bee5f136c2889348f6
Monir <monireh.taghilo@gmail.com>
2023-09-12 08:15:04 +0330

    add traffic-light.ts

---
+++ traffic-light.ts
@@ -0,0 +1,17 @@
+let color: string = "yellow";
+
+switch (color) {
+  case "red":
+    console.log("The Next Color Will be green");
+    color = "green";
+    break;
+  case "yellow":
+    console.log("The Next Color Will be red");
+    color = "red";
+    break;
+  case "green":
+    console.log("The Next Color Will be yellow");
+    color = "yellow";
+    break;
+    default:
+}
